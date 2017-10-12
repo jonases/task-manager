e4c3875f90223cbf469a91f39d34a4db82c79526
Jonas Stein <jonastei@ie.ibm.com>
2017-10-12 19:02:46 +0100

    added travis file

--- webapp.go
+++ webapp.go
@@ -14,7 +14,13 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
 	// io.WriteString(w, "This is an example server.\n")
 }
 
+func root(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+        w.Write([]byte("This is the root.\n\n\n\n\t\t\tWelcome!"))
+}
+
 func main() {
+	http.HandleFunc("/", root)
 	http.HandleFunc("/hello", HelloServer)
 	err := http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
 	if err != nil {
